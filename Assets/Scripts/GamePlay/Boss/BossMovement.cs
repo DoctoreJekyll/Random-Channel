78cea1783048f0b0535aedbd07b05a13c4f49f2e
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2024-02-06 14:44:00 +0100

    Add: Dotween

---
+++ Assets/Scripts/GamePlay/Boss/BossMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace GamePlay.Boss
+{
+    public class BossMovement : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
